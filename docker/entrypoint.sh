22b27ec29c7cf669dbf32d11126ae7f9b549ba9b
Suat Özgür <s.oezguer@gmail.com>
2023-11-07 23:03:47 +0100

    rename CQ_BRANDING_KEY

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -1,3 +1,12 @@
 #!/bin/sh
-WS_BRANDING_KEY="${WS_BRANDING_KEY:-undefined}"
+
+if [ -n "$WS_BRANDING_KEY" ]; then
+  WS_BRANDING_KEY="$WS_BRANDING_KEY"
+elif [ -n "$CQ_BRANDING_KEY" ]; then
+  # legacy var name
+  WS_BRANDING_KEY="$CQ_BRANDING_KEY"
+else
+  WS_BRANDING_KEY="undefined"
+fi
+
 sed -i "s!{{WS_BRANDING_KEY}}!${WS_BRANDING_KEY}!g" /usr/local/etc/haproxy/haproxy.cfg
